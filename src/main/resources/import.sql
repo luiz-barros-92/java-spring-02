695c25ae756a12cb4f136e826430cd5d5a867b8b
Luiz Barros <lck.barros@gmail.com>
2025-05-18 15:08:32 -0300

    Inserting data in database

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,11 @@
 INSERT INTO tb_department (name) VALUES ('Tech');
 INSERT INTO tb_department (name) VALUES ('Pet');
+INSERT INTO tb_department (name) VALUES ('Books');
 
 INSERT INTO tb_product (name, price, department_id) VALUES ('Macbook Pro', 4000.00, 1);
 INSERT INTO tb_product (name, price, department_id) VALUES ('PC Gamer', 5000.00, 1);
 INSERT INTO tb_product (name, price, department_id) VALUES ('Pet House', 300.00, 2);
+INSERT INTO tb_product (name, price, department_id) VALUES ('Samsung Monitor', 700.00, 1);
+INSERT INTO tb_product (name, price, department_id) VALUES ('Just For Fun - Torvalds', 50.00, 3);
+INSERT INTO tb_product (name, price, department_id) VALUES ('1984 - Orwell', 38.00, 3);
+INSERT INTO tb_product (name, price, department_id) VALUES ('Pet Food', 10.00, 2);
