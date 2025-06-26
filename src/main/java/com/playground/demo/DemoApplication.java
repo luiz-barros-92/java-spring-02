12ee5aaf9f8545d0172cf7115687b93103d62d36
Luiz Barros <lck.barros@gmail.com>
2025-06-26 20:31:39 -0300

    fix: starting msg with CommandLineRunner

--- src/main/java/com/playground/demo/DemoApplication.java
+++ src/main/java/com/playground/demo/DemoApplication.java
@@ -1,15 +1,18 @@
 package com.playground.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-		
-		System.out.println("Playground online!");
+		SpringApplication.run(DemoApplication.class, args);		
 	}
-
+	
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Server ON!");
+	}
 }
