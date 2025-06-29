6df2c6a98a9b8401dcf3023edc654119e0dcf1e4
Luiz Barros <lck.barros@gmail.com>
2025-06-29 14:12:22 -0300

    feat: create ProductService insert method

---
+++ src/main/java/com/playground/demo/services/ProductService.java
@@ -0,0 +1,33 @@
+package com.playground.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.playground.demo.dto.ProductDepartmentDTO;
+import com.playground.demo.entities.Department;
+import com.playground.demo.entities.Product;
+import com.playground.demo.repositories.DepartmentRepository;
+import com.playground.demo.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	@Autowired
+	private DepartmentRepository departmentRepository;
+	
+	public ProductDepartmentDTO insert (ProductDepartmentDTO dto) {		
+		Product entity = new Product();
+		entity.setName(dto.getName());
+		entity.setPrice(dto.getPrice());
+		
+		Department department = departmentRepository.findById(dto.getDepartment().getId()).orElseThrow(() -> new RuntimeException("Department not found"));			
+		entity.setDepartment(department);
+		
+		entity = repository.save(entity);
+		
+		return new ProductDepartmentDTO(entity);
+	}
+}
