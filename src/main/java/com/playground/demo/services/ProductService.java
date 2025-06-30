9d5a9ae24d11cea4fffb668adc7123128edad93b
Luiz Barros <lck.barros@gmail.com>
2025-06-30 20:30:34 -0300

    feat: create findAll() in ProductService

--- src/main/java/com/playground/demo/services/ProductService.java
+++ src/main/java/com/playground/demo/services/ProductService.java
@@ -1,8 +1,12 @@
 package com.playground.demo.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.playground.demo.dto.ProductDTO;
 import com.playground.demo.dto.ProductDepartmentDTO;
 import com.playground.demo.entities.Department;
 import com.playground.demo.entities.Product;
@@ -12,19 +16,23 @@ import com.playground.demo.repositories.ProductRepository;
 public class ProductService {
 
 	@Autowired
-	private ProductRepository repository;	
+	private ProductRepository repository;
 	
+	public List<ProductDTO> findAll() {
+		List<Product> result = repository.findAll();
+		List<ProductDTO> dto = result.stream().map(x -> new ProductDTO(x)).toList();
+		return dto;
+	}		
+	
+	@Transactional
 	public ProductDepartmentDTO insert (ProductDepartmentDTO dto) {		
 		Product entity = new Product();
 		entity.setName(dto.getName());
-		entity.setPrice(dto.getPrice());
-		
+		entity.setPrice(dto.getPrice());		
 		Department department = new Department();
 		department.setId(dto.getDepartment().getId());
-		entity.setDepartment(department);
-		
-		entity = repository.save(entity);
-		
+		entity.setDepartment(department);		
+		entity = repository.save(entity);		
 		return new ProductDepartmentDTO(entity);
 	}
 }
