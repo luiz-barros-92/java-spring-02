72f78f48c4d89f80d111805cc267d46a6bb2cff2
Luiz Barros <lck.barros@gmail.com>
2025-07-04 20:58:27 -0300

    feat: insert Category into ProductService

--- src/main/java/com/playground/demo/services/ProductService.java
+++ src/main/java/com/playground/demo/services/ProductService.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.playground.demo.dto.CategoryDTO;
 import com.playground.demo.dto.ProductDTO;
+import com.playground.demo.entities.Category;
 import com.playground.demo.entities.Department;
 import com.playground.demo.entities.Product;
 import com.playground.demo.repositories.ProductRepository;
@@ -31,7 +33,12 @@ public class ProductService {
 		entity.setPrice(dto.getPrice());		
 		Department department = new Department();
 		department.setId(dto.getDepartmentId());
-		entity.setDepartment(department);		
+		entity.setDepartment(department);
+		for (CategoryDTO catDto : dto.getCategories()) {
+			Category cat = new Category();
+			cat.setId(catDto.getId());
+			entity.getCategories().add(cat);
+		}
 		entity = repository.save(entity);
 		return new ProductDTO(entity);
 	}
